101831cb69261eeb9e15ce8e64df1a0655fcac49
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-12-04 10:39:35 +0100

    Cover missing .env variables

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
 
 class BinanceController extends Controller
 {
+    protected $binance_api_key;
+    protected $binance_secret_key;
+
+    public function __construct()
+    {
+        $this->binance_api_key = env('BINANCE_API_KEY');
+        $this->binance_secret_key = env('BINANCE_SECRET_KEY');
+
+        if(empty($this->binance_api_key) || empty($this->binance_secret_key)) {
+            throw new Exception("BINANCE_API_KEY and BINANCE_SECRET_KEY are required");
+        }
+    }
+
     public function index()
     {
         return response()->json(['Binance controller']);
--- tests/Feature/BinanceControllerTest.php
+++ tests/Feature/BinanceControllerTest.php
@@ -5,10 +5,21 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Exception;
 
 class BinanceControllerTest extends TestCase
 {
-    public function test_example()
+    public function test_it_throws_an_exception_if_env_are_missing()
+    {
+        putenv('BINANCE_API_KEY=');
+        putenv('BINANCE_SECRET_KEY=');
+
+        $response = $this->get('/api/all-orders');
+        
+        $response->assertStatus(500);
+    }
+
+    public function test_it_returns_binance_controller()
     {
         $response = $this->get('/api/all-orders');
 
